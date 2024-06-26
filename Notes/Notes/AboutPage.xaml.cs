101596ba7569f129372647571c834fabd75f77a5
Maxim Ivlev <nmaivlev@amazon.com>
2024-06-26 18:36:24 +0100

    Add initialize component

--- Notes/Notes/AboutPage.xaml.cs
+++ Notes/Notes/AboutPage.xaml.cs
@@ -6,8 +6,12 @@ using System.Threading.Tasks;
 
 namespace Notes;
 
-public partial class AboutPage
+public partial class AboutPage : ContentPage
 {
+    public AboutPage()
+    {
+        InitializeComponent();
+    }
     
     private async void LearnMore_Clicked(object sender, EventArgs e)
     {
