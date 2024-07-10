e3469a30d9575e73c2e0ce2086d0f9697e90f0b8
Max <mxmivlev@gmail.com>
2024-07-10 10:38:33 +0100

    Renaming to HaulageApp

---
+++ HaulageApplication/HaulageApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace HaulageApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
