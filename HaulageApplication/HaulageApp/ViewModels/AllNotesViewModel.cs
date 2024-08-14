a06a56c76ec71126804165030d63aa7562c5b73a
Tiphaine Brydniak <brydntip@amazon.com>
2024-08-14 13:07:30 +0100

    [tidy] remove unneeded dependencies ahead of merge

--- HaulageApplication/HaulageApp/ViewModels/AllNotesViewModel.cs
+++ HaulageApplication/HaulageApp/ViewModels/AllNotesViewModel.cs
@@ -1,11 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Input;
 using CommunityToolkit.Mvvm.Input;
 using HaulageApp.Data;
-using HaulageApp.Models;
 using Microsoft.Extensions.Logging;
 using System.Collections.ObjectModel;
 
